3641729cee5a2dd1988aa4b501514189fe4f6d8a
Omereshone Kelvin Oghenerhoro <kelvinomereshone@gmail.com>
2018-12-14 13:47:24 +0100

    urlScema pre save defined

--- server.js
+++ server.js
@@ -27,6 +27,24 @@ var urlSchema = new mongoose.Schema({
   created_at: ''
 });
 
+urlSchema.pre('save', function(next) {
+  console.log('running pre-save');
+  var doc = this;
+  Counter.findByIdAndUpdate({ _id: 'url_count' }, { $inc: { count: 1 } }, function(err, counter) {
+      if(err) return next(err);
+      console.log(counter);
+      console.log(counter.count);
+      doc._id = counter.count;
+      doc.created_at = new Date();
+      console.log(doc);
+      next();
+  });
+});
+
+mongoose.connect(process.env.MONGOLAB_URI, {
+  useMongoClient: true
+});
+
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
